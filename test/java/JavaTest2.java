f5279b025772dc8d97cab5e012e5e42baa55492c
beautiyj <beautiyj@naver.com>
2026-03-17 17:18:19 +0900

    0317 17:18

--- test/java/JavaTest2.java
+++ test/java/JavaTest2.java
@@ -1,3 +1,4 @@
+package test.java;
 /*
 * temp를 활용하여 두 변수 값을 교환하기
 * */
@@ -12,5 +13,15 @@ public class JavaTest2 {
         x = y;        // y의 값을 x에 저장 (원래 y의 값). x = y = 5
         y = temp;     // y에 temp의 값을 저장 (원래 x의 값). y= temp = 3
         System.out.println("After swap: x = " + x + ", y = " + y);
+
+//        int v1 =15;
+//
+//        if(v1>10) {
+//            int v2;
+//            v2 = v1-10; // if문 안에서 선언된 v2 지역변수는 if문 밖에서는 사용할 수 없
+//
+//        }
+//        int v3 = v1+v2+5;
     }
 }
+
