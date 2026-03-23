3ebf71f79c4f136cbeb29141216a263d6bce09cb
beautiyj <beautiyj@naver.com>
2026-03-23 12:50:40 +0900

    0323 12:50

--- task/java/JavaTask13.java
+++ task/java/JavaTask13.java
@@ -1,12 +1,73 @@
 package task.java;
 
+import java.util.Scanner;
+
 // 반복문 - while문
 public class JavaTask13 {
 
+    // 1. 문자열 반복 출력
     public static void main(String[] ar) {
-
-
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i + "문자출력");
+            i++;
+        }
+        System.out.println();
+        main2(ar);
     }
 
+    // 2. while문과 if-else문을 활용해 1~100까지의 홀.짝수 합 구하기
+    public static void main2(String[] ar) {
+        int i = 0, oddSum = 0, evenSum = 0;
 
+        while (i <= 100) {
+
+            if (i % 2 == 0) {
+                oddSum += i;
+            } else {
+                evenSum += i;
+            }
+
+            i++;
+        }
+        System.out.println(oddSum);
+        System.out.println(evenSum);
+        System.out.println();
+
+        main3(ar);
+    }
+
+    // 3. 키보드로 입력받은 숫자의 구구단을 출력하기
+    public static void main3(String[] ar) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("정수 하나를 입력하세요");
+        int n = sc.nextInt();
+
+        int i = 1;
+        while (i <= 9) {
+            System.out.println(n + "*" + i + "=" + (n * i));
+            i++;
+        }
+        System.out.println();
+
+        main4(ar);
+    }
+
+    // 4. 구구단 2~9단 출력하기
+    public static void main4(String[] ar) {
+        int n = 2, i;
+
+        while (n <= 9) {
+            System.out.println(n + "단");
+            i = 1;
+
+            while (i <= 9) {
+                System.out.println(n + "*" + i + "=" + (n * i));
+                i++;
+            }
+
+            n++;
+        }
+
+    }
 }
--- task/java/JavaTask14.java
+++ task/java/JavaTask14.java
@@ -1,6 +1,81 @@
 package task.java;
 
-// 반복문 - do while문
+/*
+// 반복문 - do while문 (파이썬엔 x 코틀린엔 동일하게 존재함)
+    보기 불편해서 잘 쓰진 않음
+
+    do {
+        반복 실행 문장;
+    } while(조건식);
+
+*/
+
+import java.util.Scanner;
 
 public class JavaTask14 {
+
+    // 1. 문자열 반복 출력
+    public static void main(String[] ar) {
+        int i =1;
+        do {
+            System.out.println(i+"do-while문 문장출력");
+            i++;
+        } while (i<=10);
+
+        System.out.println();
+        main2(ar);
+    }
+
+    // 2. 1~100까지의 홀수 짝수 합 구하기
+    public static void main2(String[] ar) {
+        int i =1, oddSum = 0, evenSum=0;
+        do {
+            if (i%2==0) {
+                oddSum += i;
+            } else {
+                evenSum += i;
+            }
+            i++;
+        } while (i<=100);
+
+        System.out.println(oddSum);
+        System.out.println(evenSum);
+
+        System.out.println();
+        main3(ar);
+    }
+
+    // 3. 종료 키워드 q가 입력 될 때까지 사용자 입력을 반복처리하는
+    // 대화형태 에코 프로그램 작성하기
+    public static void main3(String[] ar) {
+        System.out.println("메시지를 입력하세요. (종료 키워드: q");
+        Scanner sc = new Scanner(System.in);
+
+        String input;
+
+        do {
+            System.out.println(">");
+            input = sc.nextLine();
+            System.out.println("입력 메시지: "+input);
+        } while (!input.equals("q"));
+
+        System.out.println("프로그램 종료");
+    }
 }
+
+/*
+대화형 출력 시 - ars 개념
+
+    메시지를 입력하세요. (종료 키워드: q
+    >
+    가나다라
+    입력 메시지: 가나다라
+    >
+    반복
+    입력 메시지: 반복
+    >
+    q
+    입력 메시지: q
+    프로그램 종료
+
+ */
