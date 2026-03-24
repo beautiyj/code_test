5cc9b11ce4e3d1c8350350e0511f23721a495fb5
beautiyj <beautiyj@naver.com>
2026-03-24 16:52:30 +0900

    0324 16:52

--- task/java/JavaTask20.java
+++ task/java/JavaTask20.java
@@ -4,9 +4,10 @@ package task.java;
 
 public class JavaTask20 {
     // static: 정적 메소드
+    // void 넣으면 리턴값이 없다는 의미
     static void check() {
         System.out.println("메소드 호출 성공\n");
-    }   // 리턴 생략 가능함
+    }   // 리턴 생략 가능함 (return;은 가능, 리턴값을 넣는 건 불가능)
 
     // 값 전달에 의한 Call by Value 메소드 호출 방식 - 값 자체가 전달됨
     static void check(int a) {
@@ -60,6 +61,8 @@ public class JavaTask20 {
         check1();                   // 출력은 return\n
         int result = check1();
         System.out.println(result); // 출력은 return값으로 입력했던 50
+        System.out.println(check1());   // 선언 귀찮으면 이렇게/
+                                        // 출력은 return\n50 이 된다. 행위+리턴 모두 출력
 
         double result2 = check2(50, 3.14);
         System.out.println(result2);    // 53.14
