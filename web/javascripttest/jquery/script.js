a4e8bd3abb0b1198f3da34765ef05ba18898fb14
beautiyj <beautiyj@naver.com>
2026-05-20 17:47:20 +0900

    0520

---
+++ web/javascripttest/jquery/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+        // $("div").hide();                // 전부 지워짐(숨김처리)
+        $("#sample1").hide();        // 샘플1만 지워짐
+        // $("#sample2").hide();        // 샘플2만 지워짐
+
+        // document.getElementById("sample1") 자바스크립트에선 일케 가져오던 걸
+        // 제이쿼리에선 선택자로 가져오는 거.
+        // $("div").show();                // div 전부 보임
+        // $("#sample1").show();        // 순서대로 샘플1만보임 2만보임
+        // $("#sample2").show();
+      });
